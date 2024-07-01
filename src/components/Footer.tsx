a32cd9afcc4d48e31edc36229035845f7ce5e10f
MohitCode17 <mohit.codes17@gmail.com>
2024-07-01 21:33:09 +0530

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t p-5 text-center">
+      <span className="text-sm">&copy; 2024 E-Library | All Rights Reserved.</span>
+    </footer>
+  );
+};
+
+export default Footer;
